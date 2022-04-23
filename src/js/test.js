31d07591b3a421b143d1c92b7bf15723842a81bc
Andrei Smolenic <asmolenic@gmail.com>
2022-04-24 02:27:47 +0300

    Add board flipping capabilities

--- src/js/test.js
+++ src/js/test.js
@@ -2,3 +2,11 @@ import { board } from './board.js';
 import { addGlobalEventListener, qs } from './utils/dom-utils.js';
 
 board.buildBoard();
+
+addGlobalEventListener(
+  "click",
+  "#flipBoard",
+  () => {
+    qs('.board').classList.toggle('flipped');
+  }
+)
