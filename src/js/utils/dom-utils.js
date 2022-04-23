86023912459d2f5e800560d7a4ed83d82ee057e9
Andrei Smolenic <asmolenic@gmail.com>
2022-04-24 02:26:14 +0300

    Add DOM utils

---
+++ src/js/utils/dom-utils.js
@@ -0,0 +1,48 @@
+export const addGlobalEventListener = (
+  type,
+  selector,
+  callback,
+  options,
+  parent = document
+) => {
+  parent.addEventListener(
+    type,
+    e => {
+      if (e.target.matches(selector)) callback(e)
+    },
+    options
+  )
+}
+
+export const qs = (selector, parent = document) => {
+  return parent.querySelector(selector)
+}
+
+export const qsa = (selector, parent = document) => {
+  return [...parent.querySelectorAll(selector)]
+}
+
+export const createElement = (type, options = {}) => {
+  const element = document.createElement(type)
+  Object.entries(options).forEach(([key, value]) => {
+    if (key === "class") {
+      element.classList.add(value)
+      return
+    }
+
+    if (key === "dataset") {
+      Object.entries(value).forEach(([dataKey, dataValue]) => {
+        element.dataset[dataKey] = dataValue
+      })
+      return
+    }
+
+    if (key === "text") {
+      element.textContent = value
+      return
+    }
+
+    element.setAttribute(key, value)
+  })
+  return element
+}
