966f6014fc527a56faa3b5a8f4a0fa74e6542338
Andrei Smolenic <asmolenic@gmail.com>
2022-05-03 01:23:46 +0300

    FEN - add initial validation

--- src/js/board.js
+++ src/js/board.js
@@ -20,7 +20,7 @@ export const board = {
 
         if ((i + j) % 2 === 1) {
           cell.classList.add('black-cell');
-        }   
+        }
 
         board.append(cell);
       });
@@ -42,5 +42,62 @@ export const board = {
     Array.from(cells)
       .reverse()
       .forEach(cell => board.append(cell));
+  },
+  loadFEN() {
+    let fen = prompt('FEN');
+    if (!fen) {
+      alert('No FEN string provided');
+      return;
+    }
+
+    console.log('FEN validation stage 1 complete');
+
+    let segments = fen.split(' ').filter(s => s.length > 0);
+    if (segments.length !== 6) {
+      alert(`Invalid FEN string provided - incorrect number of segments - provided ${segments.length}, expected 6`);
+      return;
+    }
+
+    console.log('FEN validation stage 2 complete');
+
+    let [piecePlacement, activeColor, castlingAvailability, enPassantTargetSquare, halfMoveCount, fullMoveCount] = segments;
+
+    let ranks = piecePlacement.split('/').filter(s => s.length > 0);
+    if (ranks.length !== 8) {
+      alert(`Invalid FEN string provided - incorrect number of ranks in piece placement - provided ${ranks.length}, expected 8`);
+      return;
+    }
+
+    console.log('FEN validation stage 3 complete');
+
+    const RANK_SYMBOLS = 'pnbrkqPNBRKQ12345678';
+    let invalidRankPos = ranks.findIndex(r => {
+      console.log(`checking rank "${r}"`);
+      let invalid = false;
+      let filesChecked = 0;
+      for (let i = 0; i < r.length; i++) {
+        if (!RANK_SYMBOLS.includes(r[i])) {
+          invalid = true;
+          console.log(`symbols are NOT valid`);
+          break;
+        }
+
+        console.log(`symbols are valid`);
+
+        filesChecked += isNaN(r[i]) ? 1 : +r[i];
+      }
+
+      if (filesChecked !== 8) {
+        invalid = true;
+        console.error(filesChecked < 8 ? 'rank doesn\'s cover all 8 files' : 'rank covers more than 8 files');
+      }
+
+      return invalid;
+    });
+    if (invalidRankPos !== -1) {
+      alert(`Invalid FEN string provided - rank ${invalidRankPos} ("${ranks[invalidRankPos]}") is invalid`);
+      return;
+    }
   }
+
 };
--- src/js/program.js
+++ src/js/program.js
@@ -2,6 +2,7 @@ import { board } from './board.js';
 import { addGlobalEventListener, qs, qsa } from './utils/dom-utils.js';
 
 board.buildBoard();
+board.loadFEN();
 
 addGlobalEventListener(
   "click",
@@ -10,3 +11,6 @@ addGlobalEventListener(
    board.flipBoard();
   }
 )
+
+// need code for FEN reading
+// https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
