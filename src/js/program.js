7eb501b99c7faf6e2c8804d0af6ab82db5d6bd7c
Andrei Smolenic <asmolenic@gmail.com>
2022-05-18 22:08:59 +0300

    Add barbaric reset game functionality

--- src/js/program.js
+++ src/js/program.js
@@ -3,12 +3,14 @@ import { fen } from './fen.js';
 import { addGlobalEventListener } from './utils/dom-utils.js';
 
 board.buildBoard();
+fen.load(fen.STARTING_POSITION);
+board.renderFenData(fen.data);
 
 addGlobalEventListener(
   "click",
   "#flipBoard",
   () => {
-   board.flipBoard();
+    board.flipBoard();
   }
 );
 
@@ -16,7 +18,17 @@ addGlobalEventListener(
   "click",
   "#loadFen",
   () => {
-   fen.load();
-   board.renderFenData(fen.data);
+    fen.load();
+    board.renderFenData(fen.data);
+  }
+);
+
+addGlobalEventListener(
+  "click",
+  "#resetGame",
+  () => {
+    if(confirm('Are you sure? All game progress will be lost.')) {
+      window.location.reload();
+    }
   }
 );
